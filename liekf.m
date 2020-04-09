7047a25a9751e6f83dfdc3e8258780e6ff97ac95
Tony Pan <tonypan2000@gmail.com>
2020-04-09 18:01:24 -0400

    implemented propagation, still need init and models

--- liekf.m
+++ liekf.m
@@ -1,26 +1,37 @@
 classdef liekf < handl
     
     properties
-        A;              % error dynamics matrix
-        H;              % measurement error matrix
-        f;              % process model
-        X;              % state vector
-        P;              % state covariance
-        Q;              % input noise covariance
-        N;              % measurement noise covariance
+        mu;                 % Pose Mean
+        Sigma;              % Pose Sigma
+        gfun;               % Motion (process) model function
+        mu_pred;            % Mean after prediction step
+        Sigma_pred;         % Sigma after prediction step
+        mu_cart;
+        sigma_cart;
+        M;                  % Motion model noise covariance function
+        Q;                  % Measurement model noise covariance
     end
     
     methods
-        function obj = liekf(system)
+        function obj = liekf(init_mu, init_sigma)
             % liekf Construct an instance of this class
-            %   Input: system          - system and noise models
-            obj.A = system.A;
-            obj.f = system.f;
-            obj.H = system.H;
-            obj.Q = system.Q;
-            obj.N = system.N;
-            obj.X = eye(3);
-            obj.P = 0.1*eye(3);
+            %   Input: init          - motion and noise models
+            obj.gfun = @(mu, u) ...
+                [mu(1)+(-u(1)/u(2)*sin(mu(3))+u(1)/u(2)*sin(mu(3)+u(2)));
+                mu(2)+(u(1)/u(2)*cos(mu(3))-u(1)/u(2)*cos(mu(3)+u(2)));
+                mu(3)+u(2)+u(3)];
+            obj.mu = init_mu;
+            obj.Sigma = init_sigma;
+            % Motion noise (in odometry space, Table 5.5, p.134 in book).
+            % variance of noise proportional to alphas
+            alphas = [0.00025 0.00005 0.0025 0.0005 0.0025 0.0005].^2; 
+            obj.M = @(u) [alphas(1)*u(1)^2+alphas(2)*u(2)^2, 0, 0;
+                        0, alphas(3)*u(1)^2+alphas(4)*u(2)^2, 0;
+                        0, 0, alphas(5)*u(1)^2+alphas(6)*u(2)^2];
+            % std. of Gaussian sensor noise (independent of distance)
+            beta = deg2rad(5);
+            obj.Q = [beta^2,    0;
+                    0,      0.5^2];
         end
         
         function AdX = Ad(obj, X)
@@ -29,7 +40,7 @@ classdef liekf < handl
            AdX = [X(1:2,1:2), [X(2,3); -X(1,3)]; 0 0 1];
         end
         
-        function xhat = wedge(obj,x)
+        function xhat = wedge(obj, x)
             % TODO
             % wedge operation for se(2) to put an R^3 vector into the Lie
             % algebra basis.
@@ -45,14 +56,49 @@ classdef liekf < handl
             xhat = G1 * x(1) + G2 * x(2) + G3 * x(3);
         end
         
+        function H = posemat(state)
+            x = state(1);
+            y = state(2);
+            z = state(3);
+            roll = state(4);
+            pitch = state(5);
+            yaw = state(6);
+            vx = state(7);
+            vy = state(8);
+            vz = state(9);
+            % TODO: figure order of pitch and yaw
+            euler_angle = [yaw pitch roll];
+            rot_mat = eul2rotm(euler_angle);
+            % construct a SE(3) matrix element
+            H = [...
+                rot_mat(1,1) rot_mat(1,2) rot_mat(1,3) vx x;
+                rot_mat(2,1) rot_mat(2,2) rot_mat(2,3) vy y;
+                rot_mat(3,1) rot_mat(3,2) rot_mat(3,3) vz z;
+                0            0            0            1  0
+                0            0            0            0  1];
+        end
+        
         function prediction(obj, u)
-            % EKF propagation (prediction) step
-            obj.P = obj.A * obj.P * obj.A' + ...
-                obj.Ad(obj.X) * obj.Q * obj.Ad(obj.X)';
-            obj.X = obj.f(obj.X,u);
+            % Formulate Adjoint function to be used in propagation
+            % Convert motion command into lie algebra element to pass 
+            % in to propagation
+            % TODO: figure out order of roll and yaw
+            [yaw, pitch, roll] = rotm2eul(obj.mu(1:3, 1:3));
+            obj.mu_cart = [obj.mu(1,5); obj.mu(2,5); obj.mu(3,5); ...
+                           roll; pitch; yaw; ...
+                           obj.mu(1,4); obj.mu(2,4); obj.mu(3,4);];
+            x_k1 = obj.gfun(obj.mu_cart, u);
+            twist = logm(obj.mu \ posemat(obj, x_k1));
+
+            % SE(2) propagation model; the input is u \in se(2) plus noise
+            % propagate covariance
+            obj.Sigma_pred = obj.Sigma + Ad(obj, obj.mu) * obj.M(u) * Ad(obj, obj.mu)';
+            % propagate mean
+            obj.mu_pred = obj.mu * expm(twist);
         end
         
         function correction(obj, Y1, b1, Y2, b2)
+            % TODO
             % RI-EKF correction step
             H = [obj.H(b1); obj.H(b2)]; % stack H
             H = H([1:2,4:5],:); % 4x3 matrix, remove zero rows 
